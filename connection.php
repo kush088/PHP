5af7197a3c1a4655454cfeff59896180da9e3191
kush088 <113185198+kush088@users.noreply.github.com>
2024-05-01 11:24:19 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$username = 'root';
-	$password = '';
-	$conn = new PDO( 'mysql:host=localhost;dbname=project', $username, $password );
-	if(!$conn){
-		die("Fatal Error: Connection Failed!");
-	}
-?>
